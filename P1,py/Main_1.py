823a36117341f829f2fcdbd6119b6a13910463f3
RezCode <adlifahrezaf@gmail.com>
2026-04-24 09:08:01 +0700

    Add files via upload

---
+++ P1,py/Main_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+df = pd.read_csv("energy_sensor_data.csv")
+
+df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+
+df['Net_Profit_IDR'] = (df['Energy_Output']*2000) - (df['Maintenance_Cost']) 
+
+print(df.info())
+#print(df.head(3))
